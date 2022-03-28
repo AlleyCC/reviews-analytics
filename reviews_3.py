185220d3a9803ef84472ee1a40e0f6b62904aa9c
AlleyChang <yifanchang0602@gmail.com>
2022-03-28 20:35:40 +0800

    Add filter

---
+++ reviews_3.py
@@ -0,0 +1,23 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		count += 1 #count = count + 1
+		data.append(line)
+		if count % 1000 == 0:
+			print(len(data))
+print('檔案讀取完成,共有',len(data),'筆資料')
+ 
+sum_len = 0
+for d in data:
+	sum_len += len(d)	
+print('每筆留言的平均長度為', sum_len / len(d))
+
+
+new = []
+for d in data:
+ 	if len(d) < 100:
+ 		new.append(d)
+print('Total:', len(new),'筆留言小於100字。')
+print(new[0])
+print(new[15])
